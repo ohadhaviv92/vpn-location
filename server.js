02d58f39f4904d2fb72abb2a030ff88d6cdffe8b
ohadhaviv92 <ohadhaviv92@gmail>
2024-09-24 21:25:57 +0300

    location

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
-const express = require("express");
+import express from "express";
+import { locations } from "./location.js";
+import { users } from "./users.js";
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+let userIndex = 0;
+
+let locationIndex = 0;
+
+setInterval(() => {
+  locationIndex = 0;
+}, 1000 * 61);
+
 app.get("/", (req, res) => {
   res.send("Hello from Node.js!");
+});
+
+app.get("/location", (req, res) => {
+  const userData = users[userIndex++ % users.length];
+  const location = locations[locationIndex++ % locations.length];
+  res.json({
+    location,
+    user: userData,
+  });
 });
 
 app.listen(PORT, () => {
