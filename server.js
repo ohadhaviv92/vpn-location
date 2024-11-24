a5b72d661faf3674ab6448760467fe2e227034e2
ohadhaviv92 <ohadhaviv92@gmail>
2024-11-24 09:55:31 +0200

    fix: data location

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ let usersArr = [];
 const app = express();
 const PORT = process.env.PORT || 3002;
 
-let userIndex = 0;
 let locationIndex = 0;
 
 let nordLocationIndex = 0;
@@ -49,30 +48,6 @@ app.get("/listItem/:sessionID", async (req, res) => {
     return;
   }
   res.status(200).json({ message: "Item listed" });
-});
-
-app.post("/addUserSession", (req, res) => {
-  const { username, cookie, localStorage } = req.body;
-  if (username && cookie && localStorage) {
-    const userToUpdate = usersArr.find((user) => user.username === username);
-    if (userToUpdate) {
-      const expiryDate = new Date();
-      expiryDate.setDate(expiryDate.getDate() + 7);
-      userToUpdate.session = {
-        cookie,
-        localStorage,
-        sessionExpiry: expiryDate,
-      };
-      res.status(200).json({
-        message: "User session updated successfully",
-        user: userToUpdate,
-      });
-    } else {
-      res.status(404).json({ message: "User not found" });
-    }
-  } else {
-    res.status(400).json({ message: "Invalid user data" });
-  }
 });
 
 async function init() {
@@ -121,46 +96,208 @@ setInterval(() => {
   ).catch((error) => {
     console.error("Error saving users to file:", error);
   });
-  nordLocationIndex = 0;
 }, 1000 * 60 * 120);
 
 app.get("/", (req, res) => {
   res.send("Hello from Node.js!");
 });
 
-app.get("/location", (req, res) => {
-  const userData = usersArr[userIndex++ % usersArr.length];
-  const location = locations[locationIndex++ % locations.length];
+app.get("/broken-location", (req, res) => {
+  res.send(
+    locations.filter((location) => {
+      return location.block || location.userNotLogin;
+    })
+  );
+});
 
-  if (userData.session && userData.session.sessionExpiry) {
-    const expiryDate = new Date(userData.session.sessionExpiry);
-    if (expiryDate < new Date()) {
-      userData.session = null;
+app.get("/broken-location-nordvpn", (req, res) => {
+  res.send(
+    locationsNord.filter((location) => {
+      return location.block || location.userNotLogin;
+    })
+  );
+});
+
+app.get("/location", (req, res) => {
+  let { prevLocationName, rateLimitReset, block, userNotLogin } = req.query;
+  let userIndex;
+  if (block || userNotLogin) {
+    const indexBlock = locations.findIndex(
+      (location) => location.name === prevLocationName
+    );
+    if (indexBlock !== -1) {
+      if (block) {
+        locations[indexBlock].block = true;
+      } else {
+        locations[indexBlock].userNotLogin = true;
+      }
+      delete locations[indexBlock].rateLimitReset;
     }
   }
-  res.json({
-    location,
-    user: userData,
-    session: userData.session,
+  if (prevLocationName && rateLimitReset) {
+    rateLimitReset = parseInt(rateLimitReset);
+    const locationIndex = locations.findIndex(
+      (location) => location.name === prevLocationName
+    );
+    if (locationIndex !== -1) {
+      locations[locationIndex].rateLimitReset = rateLimitReset;
+    }
+  }
+  const now = Date.now();
+  let locationRes;
+  const locationIndex = locations.findIndex((location) => {
+    return (
+      location.rateLimitReset < now && !location.block && !location.userNotLogin
+    );
   });
+  if (locationIndex !== -1) {
+    locations[locationIndex].rateLimitReset += 1000 * 60 * 60;
+    locationRes = locations[locationIndex].name;
+    userIndex = locationIndex;
+  } else {
+    const index = locations.findIndex((location) => {
+      return (
+        location.rateLimitReset === undefined &&
+        !location.block &&
+        !location.userNotLogin
+      );
+    });
+    if (index !== -1) {
+      locations[index].rateLimitReset = now + 1000 * 60 * 60;
+      locationRes = locations[index].name;
+      userIndex = index;
+    }
+  }
+  if (!locationRes) {
+    res.json({
+      error: "No available location",
+    });
+  } else {
+    const userData = usersArr[userIndex % usersArr.length];
+    if (userData.session && userData.session.sessionExpiry) {
+      const expiryDate = new Date(userData.session.sessionExpiry);
+      if (expiryDate < new Date()) {
+        userData.session = null;
+      }
+    }
+    res.json({
+      location: locationRes,
+      user: userData,
+      session: userData.session,
+    });
+  }
+});
+
+app.get("/available-locations-nordvpn", (req, res) => {
+  const arr = locationsNord.filter(
+    (location) => location.rateLimitReset < Date.now()
+  );
+  res.json(arr);
+});
+
+app.get("/available-locations", (req, res) => {
+  const arr = locations.filter(
+    (location) => location.rateLimitReset < Date.now()
+  );
+  res.json(arr);
+});
+
+app.get("/location/all", (req, res) => {
+  res.json(locations);
 });
 
 app.get("/location-nordvpn", (req, res) => {
-  const userData = usersArr[userIndex++ % usersArr.length];
-  const location =
-    locationsNord[nordLocationIndex++ % locationsNord.length].command;
-
-  if (userData.session && userData.session.sessionExpiry) {
-    const expiryDate = new Date(userData.session.sessionExpiry);
-    if (expiryDate < new Date()) {
-      userData.session = null;
+  let { prevLocationName, rateLimitReset, block, userNotLogin } = req.query;
+  let userIndex;
+  if (block || userNotLogin) {
+    const indexBlock = locationsNord.findIndex(
+      (location) => location.command === prevLocationName
+    );
+    if (locationIndex !== -1) {
+      if (block) {
+        locationsNord[indexBlock].block = true;
+      } else {
+        locationsNord[indexBlock].userNotLogin = true;
+      }
+      delete locationsNord[indexBlock].rateLimitReset;
     }
   }
-  res.json({
-    location,
-    user: userData,
-    session: userData.session,
+  if (prevLocationName && rateLimitReset) {
+    rateLimitReset = parseInt(rateLimitReset);
+    const locationIndex = locationsNord.findIndex(
+      (location) => location.command === prevLocationName
+    );
+    if (locationIndex !== -1) {
+      locationsNord[locationIndex].rateLimitReset = rateLimitReset;
+    }
+  }
+  const now = Date.now();
+  let locationRes;
+  const locationIndex = locationsNord.findIndex((location) => {
+    return (
+      location.rateLimitReset < now && !location.block && !location.userNotLogin
+    );
   });
+  if (locationIndex !== -1) {
+    locationsNord[locationIndex].rateLimitReset += 1000 * 60 * 60;
+    locationRes = locationsNord[locationIndex].command;
+    userIndex = locationIndex;
+  } else {
+    const index = locationsNord.findIndex((location) => {
+      return (
+        location.rateLimitReset === undefined &&
+        !location.block &&
+        !location.userNotLogin
+      );
+    });
+    if (index !== -1) {
+      locationsNord[index].rateLimitReset = now + 1000 * 60 * 60;
+      locationRes = locationsNord[index].command;
+      userIndex = index;
+    }
+  }
+  if (!locationRes) {
+    res.json({
+      error: "No available location",
+    });
+  } else {
+    const userData = usersArr[userIndex % usersArr.length];
+    if (userData.session && userData.session.sessionExpiry) {
+      const expiryDate = new Date(userData.session.sessionExpiry);
+      if (expiryDate < new Date()) {
+        userData.session = null;
+      }
+    }
+    res.json({
+      location: locationRes,
+      user: userData,
+      session: userData.session,
+    });
+  }
+});
+
+app.post("/addUserSession", (req, res) => {
+  const { username, cookie, localStorage } = req.body;
+  if (username && cookie && localStorage) {
+    const userToUpdate = usersArr.find((user) => user.username === username);
+    if (userToUpdate) {
+      const expiryDate = new Date();
+      expiryDate.setDate(expiryDate.getDate() + 7);
+      userToUpdate.session = {
+        cookie,
+        localStorage,
+        sessionExpiry: expiryDate,
+      };
+      res.status(200).json({
+        message: "User session updated successfully",
+        user: userToUpdate,
+      });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } else {
+    res.status(400).json({ message: "Invalid user data" });
+  }
 });
 
 init();
