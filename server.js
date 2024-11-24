eca06aad2bddae310dd02a8ec32d7dba681499d9
ohadhaviv92 <ohadhaviv92@gmail>
2024-11-24 17:49:59 +0200

    fix: rate limit 2

--- server.js
+++ server.js
@@ -151,7 +151,7 @@ app.get("/location", (req, res) => {
     );
   });
   if (locationIndex !== -1) {
-    locations[locationIndex].rateLimitReset += 1000 * 60 * 5;
+    locations[locationIndex].rateLimitReset += 1000 * 60 * 65;
     locationRes = locations[locationIndex].name;
     userIndex = locationIndex;
   } else {
@@ -163,7 +163,7 @@ app.get("/location", (req, res) => {
       );
     });
     if (index !== -1) {
-      locations[index].rateLimitReset = now + 1000 * 60 * 5;
+      locations[index].rateLimitReset = now + 1000 * 60 * 65;
       locationRes = locations[index].name;
       userIndex = index;
     }
@@ -239,7 +239,7 @@ app.get("/location-nordvpn", (req, res) => {
     );
   });
   if (locationIndex !== -1) {
-    locationsNord[locationIndex].rateLimitReset += 1000 * 60 * 60;
+    locationsNord[locationIndex].rateLimitReset += 1000 * 60 * 65;
     locationRes = locationsNord[locationIndex].command;
     userIndex = locationIndex;
   } else {
@@ -251,7 +251,7 @@ app.get("/location-nordvpn", (req, res) => {
       );
     });
     if (index !== -1) {
-      locationsNord[index].rateLimitReset = now + 1000 * 60 * 60;
+      locationsNord[index].rateLimitReset = now + 1000 * 60 * 65;
       locationRes = locationsNord[index].command;
       userIndex = index;
     }
